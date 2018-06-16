525995254c98c2616354101c322a6fcf7b1a1642
Cole Nelson <nelso345@purdue.edu>
2018-06-16 14:28:51 -0400

    Removing extraneous versions

--- version/function_mqtt.py
+++
@@ -1,9 +0,0 @@
-#Import Modules
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client()
-
-def on_connect(client,userdata,flags,rc):
-    #define callback
-    if rc == 0:
-        print("Connection Successful")
