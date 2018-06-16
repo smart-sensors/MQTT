894fa0ca7c783e9a1dfb60a6463bad90ba11b2a9
Cole Nelson <nelso345@purdue.edu>
2018-06-16 15:34:32 -0400

    Initial revision complete --> Needs testing

--- deserialize.py
+++ deserialize.py
@@ -4,10 +4,12 @@ def deserialize(bitstream):
     DATA = 1
     NAME = 2
 
+    bitstream = ''.join(bitstream)
+
     # split encoded message
-    tokens = bitstream.split("5F")
-    sensor_type = bytearray.fromhex(tokens[SENS_TYPE]).decode()
-    sensor_name = bytearray.fromhex(tokens[NAME]).decode()
+    tokens = bitstream.split("_")
+    sensor_type = tokens[SENS_TYPE]  # bytearray(tokens[SENS_TYPE]).decode()
+    sensor_name = tokens[NAME]  # bytearray(tokens[NAME]).decode()
     data_bytes = bytearray.fromhex(tokens[DATA])
 
     # rebuild data by reversing 'endianness'
@@ -25,10 +27,9 @@ def voltmeter2string(sensor_tuple):
     return "Voltmeter {}: {} Volts".format(sensor_tuple[2], sensor_tuple[1] / 1000)
 
 
-# TODO: What if data's value is 5F?
-
+# TODO: Does this actually work? gattlib documentation doesn't specify return type of 'read'
 
 
 if __name__ == "__main__":
     
-    print(voltmeter2string(deserialize("505F00FF5F51")))
+    print(voltmeter2string(deserialize(['T', 'S', 'T', '_', '5', 'A', '_', 'T'])))
--- main.py
+++ main.py
@@ -1,52 +1,72 @@
-#Import Modules
+# Import Modules
 import paho.mqtt.client as mqtt
-import function
 import time
 from bluepy import btle
+from gattlib import *
+import function
+from configure import *
 
-#Assign Variables
-TCP_ADDRESS = None
-PORT = "1883"
-TOPIC = None
-INTERVAL = None
-BLE_NAME = None
-BLE_ADDRESS = None
-CHAR_UUID = None
+# Assign Variables
+# TCP_ADDRESS = None
+# PORT = "1883"
+# TOPIC = None
+# INTERVAL = None
+# BLE_NAME = None
+# BLE_ADDRESS = None
+# CHAR_UUID = None
 
-client = mqtt.Client()
 
-#Scans for BLE Device For INTERVAL seconds
-INTERVAL = int(raw_input("Enter BLE scaning time in secods: "))
-BLE_NAME = raw_input("Enter Device Name: ")
-BLE_ADDRESS = function.scan(INTERVAL,BLE_NAME)
+def main():
+    client = mqtt.Client()
+    device_cfg = Config()
 
-#Establish Connection with BLE Device
-function.Connect(BLE_ADDRESS)
-time.sleep(3)
-#Allows user to choose getCharacteristics
-CHAR_UUID = function.choose_CharUUID(BLE_ADDRESS,BLE_NAME)
+    # Get bluetooth address if it doesn't exist yet
+    if not device_cfg.has_settings:
+        # Scans for BLE Device For INTERVAL seconds
+        device_cfg.settings["SCAN_INTERVAL"] = input("Enter BLE scaning time in secods: ")
+        device_cfg.settings["BLE_NAME"] = input("Enter Device Name: ")
+        device_cfg.settings["BLE_ADDRESS"] = function.scan(INTERVAL, BLE_NAME)
 
-#User Input for Establishing Connection with broker
-TCP_ADDRESS = raw_input("Enter TCP Address for Broker: ")
-TOPIC = raw_input("Enter topic to publish: ")
-INTERVAL = int(raw_input("Publish every ____ seconds: "))
+    # Establish Connection with BLE Device either way
+    GATTRequester(device_cfg.settings["BLE_ADDRESS"], False)
+    time.sleep(3)  # has to wait for connection, or it'll crash
 
-#Establish Connection with Broker
-client.connect(TCP_ADDRESS,PORT)
-client.on_connect = function.on_connect
-time.sleep(3)
-client.loop_start()
-try:
-    while True:
-        #Publish message every INTERVAL second
-        MESSAGE = function.read(CHAR_UUID,BLE_ADDRESS)
-        client.publish(TOPIC,MESSAGE)
-        print("Published: %s" % MESSAGE)
-        time.sleep(INTERVAL)
-#Stop the Program by doing Ctl+c
-except KeyboardInterrupt:
-    pass
-client.loop_stop()
+    # if settings aren't there, setup the rest manually
+    if not device_cfg.has_settings:
+        device_cfg.settings["CHARACTERISTIC"] = function.choose_CharUUID(BLE_ADDRESS, BLE_NAME)
+        device_cfg.settings["TCP"] = input("Enter TCP Address for Broker: ")
+        device_cfg.settings["PORT"] = input("Enter TCP Port for Broker: ")
+        device_cfg.settings["TOPIC"] = input("Enter topic to publish: ")
+        device_cfg.settings["PUB_INTERVAL"] = input("Publish every ____ seconds: ")
 
-#Disconnects from Broker
-client.disconnect()
+    # User Input for Establishing Connection with broker
+    # TCP_ADDRESS = raw_input("Enter TCP Address for Broker: ")
+    # TOPIC = raw_input("Enter topic to publish: ")
+    # INTERVAL = int(raw_input("Publish every ____ seconds: "))
+
+    # Establish Connection with Broker
+    client.connect(device_cfg.settings["TCP"], device_cfg.settings["PORT"])
+    client.on_connect = function.on_connect
+    time.sleep(3)
+    client.loop_start()
+
+    try:
+        while True:
+            # Publish message every INTERVAL second
+            message = function.read(device_cfg.settings["CHAR_UUID"], device_cfg.settings["BLE_ADDRESS"])
+            client.publish(device_cfg.settings["TOPIC"], message)
+            print("Published: {}".format(message))
+            time.sleep(int(device_cfg.settings["PUB_INTERVAL"]))
+
+    # Stop the Program by doing Ctl+c
+    except KeyboardInterrupt:
+        pass
+    client.loop_stop()
+
+    # Disconnects from Broker
+    client.disconnect()
+    device_cfg.save_cfg()
+
+
+if __name__ == "__main__":
+    main()
