4c0b02ecbbce9db767d4f6f1f9d7acf102ed302f
Cole Nelson <nelso345@purdue.edu>
2018-06-20 03:15:06 +0000

    Working MQTT communication

--- bluepy_version/bt_main.py
+++ bluepy_version/bt_main.py
@@ -2,25 +2,50 @@
 
 # Library imports
 import os
-import paho.mqtt.client as MQTT
+import time
+import paho.mqtt.client as mqtt
 from bluepy.btle import *
 
 # Local imports
 from configure import Config
 import deserialize as ds
 import bt_functions
+# import mqtt_interface
 
 
 def main():
+    TCP = "128.10.3.51"
+    PORT = 1883
+    
+    
     device = bt_functions.le_scan(5)
     characteristic = bt_functions.selectService(device)
-    a = characteristic.read()
-    b = ds.deserialize(a)
-    print(ds.voltmeter2string(b))
-    print("done")
-    device.disconnect()
     
-
+    client = mqtt.Client("", True, None, mqtt.MQTTv311)
+    print(client)
+    client.on_connect = on_connect
+    client.connect(TCP, PORT)
+    client.loop_start()
+    time.sleep(1)
+    
+    try:
+        while True:
+            raw = characteristic.read()
+            message = ds.voltmeter2string(ds.deserialize(raw))
+            client.publish("TEST", message)
+            print("Published {} to topic {}".format(message, "TEST"))
+            time.sleep(3)
+    except KeyboardInterrupt:
+        pass
+    
+    #client = mqtt_interface.MQTTConnection(device, characteristic, "TEST", TCP, PORT)
+    #try:
+    #    client.run(3)
+    #except KeyboardInterrupt:
+    #    device.disconnect()
+    
+def on_connect(client, userdata, flags, rc):
+    print("Connected to broker")
 
 
 if __name__ == "__main__":
